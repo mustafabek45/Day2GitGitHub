4027565b181086cf1d92013debf7408ffa5f7af5
Mustafa Bek <bekmustafa45@gmail.com>
2022-08-15 17:20:56 +0200

    2.C

--- src/Day1Git.java
+++ src/Day1Git.java
@@ -2,6 +2,8 @@ public class Day1Git {
     public static void main(String[] args) {
 
         System.out.println("Hello Git 1. deneme");
+        System.out.println("Hello Git 2. deneme");
+
 
     }
 }
