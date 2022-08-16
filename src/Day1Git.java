4f5fe35be453ef82d9fb0356a236df97c2a6aae0
mustafabek45 <91026713+mustafabek45@users.noreply.github.com>
2022-08-16 21:17:08 +0200

    Update Day1Git.java

--- src/Day1Git.java
+++ src/Day1Git.java
@@ -5,6 +5,7 @@ public class Day1Git {
         System.out.println("Hello Git 2. deneme");
         System.out.println("Hello Git 3. deneme");
         System.out.println("Hello Git 4. deneme");
+        System.out.println("Hello Git 5. deneme");
 
 
 
